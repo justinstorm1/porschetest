87da3adb22f69fd5e11b52eaec139b1edee10e47
Justin-Storm <226jstorm@frhsd.com>
2025-03-20 16:25:45 -0400

    7th

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,15 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './Navbar.css'
 import porsheLogoWords from  './porscheLogoWords.png'
 import porsheLogoBadge from  './porscheLogoBadge.png'
 
 export default function Navbar() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
-    /* <nav className='navbar navbar-expand-sm navbar-dark'>
-      <div className='container'>
-        <a className='navbar-brand' href='#'>Porsche</a>
-        <button className='navbar-toggler' data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-          <span className='navbar-toggler-icon'></span>
-        </button>
-        <div className='collapse navbar-collapse' id='navbarCollapse'>
-          <ul className='navbar-nav ms-auto'>
-            <li className='nav-item'>
-              <a className='nav-link' href='#'>Home</a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='#'>About</a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='#'>Apps</a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='#'>Portfolio</a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='#'>Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav> */
+    <>
     <nav>
-      <div>
+      <div className='left' onClick={() => setIsSidebarOpen(true)}>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
         </svg>
@@ -41,9 +17,15 @@ export default function Navbar() {
       </div>
       <img className='logoWord' src={porsheLogoWords} alt='Logo' />
       <img className='logoBadge' src={porsheLogoBadge} alt='Logo' />
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
-      </svg>
+      <div className='right'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+        </svg>
+      </div>
     </nav>
+
+    <aside className={`sidebar ${isSidebarOpen ? 'open' : ''}`}></aside>
+    <div className={`overlay ${isSidebarOpen ? 'show' : ''}`} onClick={() => setIsSidebarOpen(false)}></div>
+    </>
   )
 }
